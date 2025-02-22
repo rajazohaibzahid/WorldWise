d475a2d5f08b48f80d58f14c1db7f799be58ad34
Raja Zohaib Zahid <rajazohaibzahid16@gmail.com>
2025-02-23 04:54:12 +0500

    Fixed import issue with PageNav

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,6 +1,6 @@
 // Uses the same styles as Product
 import styles from "./Product.module.css";
-import PageNav from "../../components/Pagenav";
+import PageNav from "../../components/PageNav";
 
 export default function Product() {
   return (
